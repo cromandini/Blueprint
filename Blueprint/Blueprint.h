8bf2aaa4ef5af4e265591bb59732ff614bf563dd
Claudio Romandini <cromandini@me.com>
2014-08-06 13:41:22 -0300

    Create project

---
+++ Blueprint/Blueprint.h
@@ -0,0 +1,19 @@
+//
+//  Blueprint.h
+//  Blueprint
+//
+//  Created by Claudio Romandini on 06/08/14.
+//  Copyright (c) 2014 Claudio Romandini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blueprint.
+FOUNDATION_EXPORT double BlueprintVersionNumber;
+
+//! Project version string for Blueprint.
+FOUNDATION_EXPORT const unsigned char BlueprintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blueprint/PublicHeader.h>
+
+
